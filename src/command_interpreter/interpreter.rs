845f31689409e9e90e1e63fc3692ebb32be38359
physine <aaroncleary13@gmail.com>
2025-07-18 00:25:07 +0100

    first test case

--- src/command_interpreter/interpreter.rs
+++ src/command_interpreter/interpreter.rs
@@ -9,14 +9,30 @@ pub fn interpret(state: &AppState, ctx: &Context, user_input: &str) -> Effect {
     eval(ast)
 }
 
-fn lexer(user_input: &str) -> Vec<Token> {
-    vec![Token {}]
+fn lexer(user_input: &str) -> Vec<String> {
+    vec!["(".to_owned(), "help".to_owned(), ")".to_owned()]
 }
 
-fn parse(tokens: Vec<Token>, ctx: &Context) -> AST {
+fn parse(tokens: Vec<String>, ctx: &Context) -> AST {
     AST {}
 }
 
 fn eval(ast: AST) -> Effect {
     Effect {}
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn lexer_verify() {
+        let input = "(help)";
+        let tokens = lexer(input);
+        assert_eq!(
+            tokens,
+            vec!["(".to_owned(), "help".to_owned(), ")".to_owned()]
+        );
+    }
 }
